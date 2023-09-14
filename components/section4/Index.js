524ec1641945b39c8337de14ab7ced48a21095c8
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-09-14 11:03:10 +0100

    bstack dev setup config

---
+++ components/section4/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Section 4</div>
+  )
+}
+
+export default index;
+
+// Create other Components for your section in this folder.
+// with this you won't need to touch other part of the app to avoid conflict.
